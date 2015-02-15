128235d2af3a6051d96fcc713347adec227cfcf7
AlexChar <alex.charizopoulos@gmail.com>
2015-02-15 18:23:37 +0200

    express init

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,5 +1,21 @@
 "use strict";
 
-exports.render = function(req, res){
-	res.send('Hello world');
+var express = require('express');
+
+var hasName = function(req, res, next) {
+	if(req.param('name')) {
+		next();
+	} else {
+		res.send('What is your name?');
+	}
+};
+
+var sayHello = function(req, res, next) {
+	res.send('Hello ' + req.param('name'));
 }
+
+var app = express();
+app.get('/', hasName, sayHello);
+
+app.listen(3000);
+console.log('Server running at http://localhost:3000');
